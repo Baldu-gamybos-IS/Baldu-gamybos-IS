5a88948842ca08830d2c55cc6e798d71e7f430f0
linper <llinas7879@gmail.com>
2021-05-08 19:15:40 +0300

    improved distributor model

--- Baldu-Gamybos-IS/Models/Distributor.cs
+++ Baldu-Gamybos-IS/Models/Distributor.cs
@@ -14,6 +14,8 @@ namespace Baldu_Gamybos_IS.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
 
         public virtual ICollection<GenericOrder> GenericOrders { get; set; }
     }
