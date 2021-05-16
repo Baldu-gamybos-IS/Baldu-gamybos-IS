675cb7a7d11ab9765f7a3dea645a03c259ef4f08
AAndrejevas <aandrejevas5@gmail.com>
2021-05-16 10:00:47 +0300

    formatting

--- Baldu-Gamybos-IS/Controllers/OrdersController.cs
+++ Baldu-Gamybos-IS/Controllers/OrdersController.cs
@@ -7,135 +7,142 @@ using Microsoft.EntityFrameworkCore;
 using Baldu_Gamybos_IS.Models.ViewModel.OrderView;
 using Microsoft.AspNetCore.Authorization;
 
-namespace mvc_auth_test.Controllers
-{
-    public class OrdersController : Controller
-    {
-        private readonly ILogger<OrdersController> _logger;
-        private readonly furnitureContext Context;
+namespace mvc_auth_test.Controllers {
+	public class OrdersController : Controller {
+		private readonly ILogger<OrdersController> _logger;
+		private readonly furnitureContext Context;
 
-        public OrdersController(ILogger<OrdersController> logger, furnitureContext context)
-        {
-            _logger = logger;
-            Context = context;
-        }
-        [Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
-        public IActionResult Orders()
-        {
-            if(TempData["SuccessState"]!=null){
-                ViewData["SuccessState"]=true;
-            }
-            if (TempData["SuccessfullyCreatedOrder"] != null) {
-                ViewData["SuccessfullyCreatedOrder"] = true;
-            }
-            var orders = Context.GenericOrders.Select(r => new GenericOrder{         
-                Id = r.Id,
-                Price = r.Price,
-                PayedAmount=r.PayedAmount,
-                Deadline =r.Deadline,
-                Direction =r.Direction,
-                Comment =r.Comment,
-                InitDate =r.InitDate,
-                DestAddr =r.DestAddr,
-                DestZipcode =r.DestZipcode,
-                FkStatus=r.FkStatus,
-                FkProfile=r.FkProfile,
-                FkDistributor=r.FkDistributor,
-                FkSupplier =r.FkSupplier,
-                FkStatusNavigation=r.FkStatusNavigation
-            });
-            return View(orders);
-        }
-        [Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
-        public IActionResult ShowOrder(int id){
-            var checking=Context.GenericOrders.Include(h=> h.OrderResources).
-            ThenInclude(g=>g.FkResourceNavigation).
-            Include(b=>b.Products).
-            ThenInclude(c=>c.ProductResources).ThenInclude(d=>d.FkResourceNavigation).Select(r=> new GenericOrder{
-                Id = r.Id,
-                Price = r.Price,
-                PayedAmount=r.PayedAmount,
-                Deadline =r.Deadline,
-                Direction =r.Direction,
-                Comment =r.Comment,
-                InitDate =r.InitDate,
-                DestAddr =r.DestAddr,
-                DestZipcode =r.DestZipcode,
-                FkStatus=r.FkStatus,
-                FkProfile=r.FkProfile,
-                FkDistributor=r.FkDistributor,
-                FkSupplier =r.FkSupplier,
-                FkStatusNavigation=r.FkStatusNavigation,
-                FkDistributorNavigation=r.FkDistributorNavigation,
-                FkProfileNavigation=r.FkProfileNavigation,
-                FkSupplierNavigation=r.FkSupplierNavigation,
-                Products=r.Products,
-                OrderResources=r.OrderResources,
-                
+		public OrdersController(ILogger<OrdersController> logger, furnitureContext context) {
+			_logger = logger;
+			Context = context;
+		}
 
-            });
-           
-            var ourOrder=checking.Where(r => r.Id==id).Single();
-            return View(ourOrder);
-        }
-        [Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
-        public IActionResult OrderState(int id){
-            var checking=Context.GenericOrders.Include(h=> h.OrderResources).
-            ThenInclude(g=>g.FkResourceNavigation).
-            Include(b=>b.Products).
-            ThenInclude(c=>c.ProductResources).ThenInclude(d=>d.FkResourceNavigation).Select(r=> new GenericOrder{
-                Id = r.Id,
-                Price = r.Price,
-                PayedAmount=r.PayedAmount,
-                Deadline =r.Deadline,
-                Direction =r.Direction,
-                Comment =r.Comment,
-                InitDate =r.InitDate,
-                DestAddr =r.DestAddr,
-                DestZipcode =r.DestZipcode,
-                FkStatus=r.FkStatus,
-                FkProfile=r.FkProfile,
-                FkDistributor=r.FkDistributor,
-                FkSupplier =r.FkSupplier,
-                FkStatusNavigation=r.FkStatusNavigation,
-                FkDistributorNavigation=r.FkDistributorNavigation,
-                FkProfileNavigation=r.FkProfileNavigation,
-                FkSupplierNavigation=r.FkSupplierNavigation,
-                Products=r.Products,
-                OrderResources=r.OrderResources,
-            });
-           
-            var ourOrder=checking.Where(r => r.Id==id).Single();
-            return View(ourOrder);
-        }
-        [Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
-        public IActionResult ChangeState(int id,int state){
-            var obj=Context.GenericOrders.Where(r=>r.Id==id).Single();
-            obj.FkStatus=state;
-            TempData["SuccessState"]=true;
+		[Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
+		public IActionResult Orders() {
+			if (TempData["SuccessState"] != null) {
+				ViewData["SuccessState"] = true;
+			}
+			if (TempData["SuccessfullyCreatedOrder"] != null) {
+				ViewData["SuccessfullyCreatedOrder"] = true;
+			}
+			var orders = Context.GenericOrders.Select(r => new GenericOrder {
+				Id = r.Id,
+				Price = r.Price,
+				PayedAmount = r.PayedAmount,
+				Deadline = r.Deadline,
+				Direction = r.Direction,
+				Comment = r.Comment,
+				InitDate = r.InitDate,
+				DestAddr = r.DestAddr,
+				DestZipcode = r.DestZipcode,
+				FkStatus = r.FkStatus,
+				FkProfile = r.FkProfile,
+				FkDistributor = r.FkDistributor,
+				FkSupplier = r.FkSupplier,
+				FkStatusNavigation = r.FkStatusNavigation
+			});
+			return View(orders);
+		}
 
-            Context.GenericOrders.Update(obj);
-            Context.SaveChanges();
-            return RedirectToAction("Orders","Orders");
-        }
-        [Authorize(Roles = "pirkėjas,vadybininkas")]
-        public IActionResult Order() {
-            return this.View("Order", new OrderView());
-        }
-        [Authorize(Roles = "pirkėjas,vadybininkas")]
-        [HttpPost]
-        public IActionResult CreateOrder(OrderView view) {
-            view.Order.Direction = false;
-            view.Order.PayedAmount = 0.0f;
-            view.Order.FkStatusNavigation = this.Context.OrderStatuses.Find(1);
-            view.Order.InitDate = DateTime.UtcNow;
-            //view.Files?.ForEach(file => view.Order.Files.Add(new(file, view.Order)));
+		[Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
+		public IActionResult ShowOrder(int id) {
+			var checking = Context.GenericOrders.Include(h => h.OrderResources).
+			ThenInclude(g => g.FkResourceNavigation).
+			Include(b => b.Products).
+			ThenInclude(c => c.ProductResources).ThenInclude(d => d.FkResourceNavigation).Select(r => new GenericOrder {
+				Id = r.Id,
+				Price = r.Price,
+				PayedAmount = r.PayedAmount,
+				Deadline = r.Deadline,
+				Direction = r.Direction,
+				Comment = r.Comment,
+				InitDate = r.InitDate,
+				DestAddr = r.DestAddr,
+				DestZipcode = r.DestZipcode,
+				FkStatus = r.FkStatus,
+				FkProfile = r.FkProfile,
+				FkDistributor = r.FkDistributor,
+				FkSupplier = r.FkSupplier,
+				FkStatusNavigation = r.FkStatusNavigation,
+				FkDistributorNavigation = r.FkDistributorNavigation,
+				FkProfileNavigation = r.FkProfileNavigation,
+				FkSupplierNavigation = r.FkSupplierNavigation,
+				Products = r.Products,
+				OrderResources = r.OrderResources,
 
-            this.TempData["SuccessfullyCreatedOrder"] = true;
-            //this.Context.Files.AddRange(view.Order.Files);
-            this.Context.GenericOrders.Add(view.Order);
-            this.Context.SaveChanges();
-            return this.RedirectToAction("Orders", "Orders");
-        }
-    }
+
+			});
+
+			var ourOrder = checking.Where(r => r.Id == id).Single();
+			return View(ourOrder);
+		}
+
+		[Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
+		public IActionResult OrderState(int id) {
+			var checking = Context.GenericOrders.Include(h => h.OrderResources).
+			ThenInclude(g => g.FkResourceNavigation).
+			Include(b => b.Products).
+			ThenInclude(c => c.ProductResources).ThenInclude(d => d.FkResourceNavigation).Select(r => new GenericOrder {
+				Id = r.Id,
+				Price = r.Price,
+				PayedAmount = r.PayedAmount,
+				Deadline = r.Deadline,
+				Direction = r.Direction,
+				Comment = r.Comment,
+				InitDate = r.InitDate,
+				DestAddr = r.DestAddr,
+				DestZipcode = r.DestZipcode,
+				FkStatus = r.FkStatus,
+				FkProfile = r.FkProfile,
+				FkDistributor = r.FkDistributor,
+				FkSupplier = r.FkSupplier,
+				FkStatusNavigation = r.FkStatusNavigation,
+				FkDistributorNavigation = r.FkDistributorNavigation,
+				FkProfileNavigation = r.FkProfileNavigation,
+				FkSupplierNavigation = r.FkSupplierNavigation,
+				Products = r.Products,
+				OrderResources = r.OrderResources,
+			});
+
+			var ourOrder = checking.Where(r => r.Id == id).Single();
+			return View(ourOrder);
+		}
+
+		[Authorize(Roles = "darbuotojas,pirkėjas,vadybininkas")]
+		public IActionResult ChangeState(int id, int state) {
+			var obj = Context.GenericOrders.Where(r => r.Id == id).Single();
+			obj.FkStatus = state;
+			TempData["SuccessState"] = true;
+
+			Context.GenericOrders.Update(obj);
+			Context.SaveChanges();
+			return RedirectToAction("Orders", "Orders");
+		}
+
+		[Authorize(Roles = "pirkėjas,vadybininkas")]
+		public IActionResult Order() {
+			return this.View("Order", new OrderView());
+		}
+
+		[Authorize(Roles = "pirkėjas,vadybininkas")]
+		[HttpPost]
+		public IActionResult CreateOrder(OrderView view) {
+			view.Order.Direction = false;
+			view.Order.PayedAmount = 0.0f;
+			view.Order.FkStatusNavigation = this.Context.OrderStatuses.Find(1);
+			view.Order.InitDate = DateTime.UtcNow;
+			//view.Files?.ForEach(file => view.Order.Files.Add(new(file, view.Order)));
+
+			this.TempData["SuccessfullyCreatedOrder"] = true;
+			//this.Context.Files.AddRange(view.Order.Files);
+			this.Context.GenericOrders.Add(view.Order);
+			this.Context.SaveChanges();
+			return this.RedirectToAction("Orders", "Orders");
+		}
+
+		[Authorize(Roles = "vadybininkas")]
+		public IActionResult Report() {
+			return this.View("Report", this.Context.GenericOrders);
+		}
+	}
 }
